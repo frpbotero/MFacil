f8881921e812a19f7889156bb4556c6d97e21e43
Felipe Botero <frpbotero@gmail.com>
2023-02-25 17:05:13 -0400

    Create component Post

---
+++ frontend/src/components/Posts/Post.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import * as S from "./styles";
+
+export function Post(){
+    const [comment,setComment]=useState('')
+    const [replie,setReplie]=useState(false)
+    function handleReplie(){
+        setReplie(!replie)
+    }
+
+
+    return(
+        <S.Container>     
+            <S.User src="https://avatars.githubusercontent.com/u/59348629?v=4" alt="Felipe Botero" />  
+            
+            <S.Post>
+            
+                <S.Content>
+                <S.Header>
+                    <h3>Felipe Botero</h3>
+                    <p>Desenvolvedor Front-end</p>
+                </S.Header>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, esse! Ex quo vitae illum unde doloremque, esse officiis, a quas aliquid doloribus eius ratione deleniti sequi explicabo tenetur soluta consequuntur!
+                </S.Content>
+                <S.Footer>
+                <div>
+                    <p>Gostei (4)</p> <p>3 d</p>
+                </div>
+                <div>
+
+                <button>Gostei</button>|<button onClick={handleReplie}>Responder</button>
+                </div>
+
+
+                
+                
+            </S.Footer>
+            <S.spaceResponse>
+                {replie? <form action="">
+                    <textarea name="" id="contentComments" onChange={e=>setComment(e.target.value)}/>
+                    {comment!==""? <button>Enviar</button>:""}
+                </form>:""}
+
+            </S.spaceResponse>
+            </S.Post>
+            
+            
+            
+        </S.Container>
+    )
+}
---
+++ frontend/src/components/Posts/styles.ts
@@ -0,0 +1,90 @@
+import styled from "styled-components";
+
+export const Container = styled.body`
+    width:30%;
+    display:flex;
+    align-items:top;
+   
+    
+
+`
+export const User = styled.img`
+        height:3rem;
+        margin-right:0.5rem;
+        border-radius:3rem
+    
+`
+
+
+export const Header=styled.header`
+    display:flex
+    flex-direction:column;
+    margin-bottom:0.5rem;
+
+    p{
+        font-weight:lighter
+    }
+
+`
+
+export const Post=styled.div`
+    
+
+`
+export const Content=styled.main`
+    
+    text-align:justify;
+    background-color:#cecece;
+    padding:0.5em;
+    border-radius:0.5rem
+
+`
+export const Footer=styled.footer`
+    margin-top:0.5rem;
+    display:flex;
+    flex-direction:column;
+    
+
+    button{
+        background:transparent;
+        border: 1px solid transparent;
+        padding: 0.6em 0.8em;
+        border-radius: 8px;
+        outline:0;
+        cursor:pointer;
+        margin-top:0.5rem;
+        color:#a8a8a8;
+        trasition: all 0.5s
+    }
+    button:hover{
+        background:#a8a8a8;
+        color:white;
+        cursor:pointer;
+        trasition: all 0.5s
+        
+    }
+    div{
+      display:flex;
+      justify-content:space-between;
+      padding:0 0.5rem
+    }
+
+`
+export const Botoes = styled.div`
+    display:flex
+`
+export const spaceResponse = styled.div`
+
+textarea{
+    width:100%;
+    border: 2px solid #ccc;
+    border-radius:1rem;
+    resize: none;
+    padding: 0.8rem 1.6rem;
+}
+textarea:focus{
+    width:100%;
+    border:solid transparent;
+}
+
+`
