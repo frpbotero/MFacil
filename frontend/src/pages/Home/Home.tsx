689f1267c00b9ea2e9d56f15b1dbaa7ece88f137
Felipe Botero <frpbotero@gmail.com>
2023-02-26 16:21:09 -0400

    create page Home

---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import * as S from "./styles"
+import MFacil from "../../assets/Ideia MFacil.png"
+
+export function Home(){
+    return(
+        <S.Background>
+            <img src={MFacil} alt="" />
+        </S.Background>
+    )
+}
---
+++ frontend/src/pages/Home/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+
+export const Background=styled.div`
+    img{
+        width:100%;
+        height:100vh
+    }
+    
+`
