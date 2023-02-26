72af706a9a60091033b23275d6f372a904d26f53
Felipe Botero <frpbotero@gmail.com>
2023-02-26 16:21:40 -0400

    Create page HomeUser

---
+++ frontend/src/pages/HomeUser/HomeUser.tsx
@@ -0,0 +1,14 @@
+import { Post } from "../../components/Posts/Post";
+import * as S from "./styles"
+
+
+export function HomeUser(){
+    return(
+        <S.Conteiner>
+            <Post />
+            <Post />
+        </S.Conteiner>
+    )
+
+
+}
---
+++ frontend/src/pages/HomeUser/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+
+export const Conteiner = styled.div`
+    display:flex;
+    flex-direction:column;
+    align-items:center; 
+    padding-top:0.5rem;
+    
+
+`
