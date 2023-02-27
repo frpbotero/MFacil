4188c0497c6e7130b58b4d716eac8a3e4773a7ea
Felipe Botero <frpbotero@gmail.com>
2023-02-26 20:47:48 -0400

    Update UserContext Providers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,7 +20,7 @@ function App() {
             <Route  path="/"index  element={<Home/>}/>
             <Route  path="/login" element={<Login/>}/>
             <Route  path="/register" element={<Register/>}/>
-            <Route  path="/user" element={<HomeUser/>}/>
+            <Route  path="/feed" element={<HomeUser/>}/>
             <Route  path="/handle" element={<ConfigUser/>}/>
           </Routes>
         </div>
--- frontend/src/providers/UserContext.tsx
+++ frontend/src/providers/UserContext.tsx
@@ -1,6 +1,20 @@
-import { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import {useNavigate} from "react-router-dom"
 import { api } from "../api/api";
+import { createContext } from "react";
+
+export type GlobalContent = {
+    
+  }
+export const UserContext = createContext<GlobalContent>({
+    copy: 'Hello World', // set a default value
+setCopy: () => {},
+});
+
+
+
+
+    
 
 
 export function UserProvider({children}:any){
@@ -10,14 +24,67 @@ export function UserProvider({children}:any){
     async function userSignUp(formData:any){
         try{
             const response:any = await api.post("/user",formData)
-            alert({reponse.message})
+            alert("Usuário criado com sucesso!")
             navigate("/")
         }catch{
-            alert({})
+            alert("Aconteceu algo de errado, tente novamente!")
         }
     }
+    async function userLogin(formData:any){
+        
+        try{
+            const response = await api.post("/login",formData)
+            setUser(response.data.user)
+            localStorage.setItem("user",response.data.token)
+            navigate("/feed")
+        }catch{
+            alert("Favor verificar os dados e tentar novamente!")
+        }
 
+    }
 
+    function logout(){
+        setUser(null)
+        localStorage.removeItem("user")
+        navigate("/")
+    }
+
+    useEffect(()=>{
+        const token = localStorage.getItem("user")
+
+        if(token){
+            async function verifyUserLogin(){
+                try{
+                    const response = await api.get("/profile",{
+                        headers:{
+                            Authorization: `Bearee ${token}`
+                        } 
+                    })
+                    setUser(response.data)
+                    navigate("/feed")
+                }catch(error){
+                    localStorage.removeItem("user")
+                }
+            }
+            verifyUserLogin()
+        }
+    }, [])
+
+    return(
+        <UserContext.Provider
+        value={
+            {
+                user,
+                setUser,
+                userSignUp,
+                userLogin,
+                logout
+            }
+        }>
+
+            {children}
+        </UserContext.Provider>
+    )
 
 
 }
