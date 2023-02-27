d8ff8c96eceb0429f412d71d932791bde36faac3
Felipe Botero <frpbotero@gmail.com>
2023-02-27 16:48:48 -0400

    Create comment.route.ts

---
+++ backend/src/comment/comment.route.ts
@@ -0,0 +1,13 @@
+import express from "express"
+import controller from "./comment.controller"
+
+const router = express.Router()
+
+
+router.get("/",controller.findAll)
+router.get("/:id",controller.findById)
+router.post("/",controller.create)
+router.put("/:id",controller.updateById)
+router.delete("/:id",controller.deleteByID)
+
+export = router
