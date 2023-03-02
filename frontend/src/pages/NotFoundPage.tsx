957e09500310c0c7f4dd3041ece1c328b9fd9e06
Felipe Botero <59348629+FeBotero@users.noreply.github.com>
2023-03-01 21:47:01 -0400

    delete files NotFoundPage

--- frontend/src/pages/NotFoundPage.tsx
+++
@@ -1,6 +0,0 @@
-import StyledNotFoundPage from "./notfoundstyle.js";
-
-export function NotFoundPage(){
-  return <StyledNotFoundPage>Page Not Found</StyledNotFoundPage>;
-};
-
--- frontend/src/pages/notfoundstyle.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const StyledNotFoundPage = styled.h1`
-  color: var(--gray0);
-  margin-top: 10rem;
-  text-align: center;
-`;
-
-export default StyledNotFoundPage;
